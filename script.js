fa622da38b0e2b52815ce8907cc46ee83579ca75
sheelman <sheeliah@hotmail.fr>
2017-08-03 19:26:57 +0200

    ouverture fermeture
    Alors maintenant les photo change entre le noir et blanc et la couleur et les index ce mette en place ils s'ouvrent et ce ferme

--- script.js
+++ script.js
@@ -6,17 +6,37 @@ $(function(){
         
         init = function(){
             
-            $mainMenuItems.children(".images").click(function(){
-                
+            $mainMenuItems.children(".images").click(function(){    
                 var newIndex = $(this).parent().index(),
-                $item = $mainMenuItems.eq(newIndex),
-                $colorImage = $item.find(".color");
+                $item = $mainMenuItems.eq(newIndex);
                 
-                $colorImage.animate({left:"0px"},250);
-                $item.animate({width: "420px"},250);
-                
-                openedIndex = newIndex;
+                if(openedIndex === newIndex)
+                    {
+                        animateItem($item,false, 250);
+                        openedIndex = -1;
+                    }
+                    else
+                    {
+                        if(validIndex(newIndex))
+                            {
+                                animateItem($mainMenuItems.eq(openedIndex), false,250);
+                                openedIndex = newIndex;
+                                animateItem($item, true, 250);
+                            }
+                    }
+
             });
-        };  
+        };
+        validIndex = function(indexToCheck){
+            return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems);
+        };
+        animateItem = function($item, toOpen, speed){
+            
+            var $colorImage = $item.find(".color"),
+            itemParam = toOpen ? {width:"420px"}: {width: "140px"},
+            colorImageParam = toOpen ? {left: "0px"}: {left: "140px"}; 
+            $colorImage.animate(colorImageParam,speed);
+            $item.animate(itemParam,speed);
+        };
         init();
 });
