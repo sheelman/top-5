74322cb113e3e3e1163e825f7bc4da7987a91bf4
sheelman <sheeliah@hotmail.fr>
2017-08-03 20:23:22 +0200

    voila le site est fini
    changement du jquery en refactoring, gestion des bouttton par js.

--- script.js
+++ script.js
@@ -4,41 +4,41 @@ $(function(){
         totalMainMenuItems = $mainMenuItems.length,
         openedIndex = 1,
         
-        init = function(){
-            
-            bindEvents();
-            
+        init = function(){          
+            bindEvents();            
                 if(validIndex(openedIndex))
                 {
                     animateItem($mainMenuItems.eq(openedIndex), true, 700);
                 }
-        };
+        },
+        
         bindEvents = function(){
             
             $mainMenuItems.children(".images").click(function(){    
-                var newIndex = $(this).parent().index(),
-                $item = $mainMenuItems.eq(newIndex);
+                var newIndex = $(this).parent().index();
+                checkAndAnimateItem(newIndex);
+            });
+            
+            $(".button").hover(
+            function(){
+                $(this).addClass("hovered");        
+                              
+            },
+            function(){
+                $(this).removeClass("hovered"); 
                 
-                if(openedIndex === newIndex)
-                    {
-                        animateItem($item,false, 250);
-                        openedIndex = -1;
-                    }
-                    else
-                    {
-                        if(validIndex(newIndex))
-                            {
-                                animateItem($mainMenuItems.eq(openedIndex), false,250);
-                                openedIndex = newIndex;
-                                animateItem($item, true, 250);
-                            }
-                    }
-
+            });
+            
+            $(".button").click(function(){
+                var newIndex = $(this).index();
+                checkAndAnimateItem(newIndex);
             });
         },
+        
         validIndex = function(indexToCheck){
             return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems);
-        };
+        },
+        
         animateItem = function($item, toOpen, speed){
             
             var $colorImage = $item.find(".color"),
@@ -46,6 +46,24 @@ $(function(){
             colorImageParam = toOpen ? {left: "0px"}: {left: "140px"}; 
             $colorImage.animate(colorImageParam,speed);
             $item.animate(itemParam,speed);
+        },
+        
+        checkAndAnimateItem = function(indexToCheckAndAnimate){
+             if(openedIndex === indexToCheckAndAnimate)
+             {
+                 animateItem($mainMenuItems.eq(indexToCheckAndAnimate),false, 250);
+                 openedIndex = -1;
+             }
+             else
+             {
+                 if(validIndex(indexToCheckAndAnimate))
+                 {
+                      animateItem($mainMenuItems.eq(openedIndex), false,250);
+                      openedIndex = indexToCheckAndAnimate;
+                      animateItem($mainMenuItems.eq(openedIndex), true, 250);
+                 }
+             }
         };
+            
         init();
 });
